e04b0a8a94abd4d8b2efc803388d66fbc7875d14
Malak Emad <119331267+malakemadd@users.noreply.github.com>
2025-05-20 18:54:34 +0300

    Update environment.ts

--- src/app/base/environment.ts
+++ src/app/base/environment.ts
@@ -6,16 +6,25 @@
 
 export enum Environment {
     
-   baseUrl= "https://localhost:7093/api/",
-   signalRUrl= 'https://localhost:7093/chathub',
-   notificationsUrl= 'https://localhost:7093/notification',
-   biddingUrl= 'https://localhost:7093/biddingHub'
+//    baseUrl= "https://localhost:7093/api/",
+//    signalRUrl= 'https://localhost:7093/chathub',
+//    notificationsUrl= 'https://localhost:7093/notification',
+//    biddingUrl= 'https://localhost:7093/biddingHub'
+    baseUrl= "http://prolance.runasp.net/",
+   signalRUrl= 'http://prolance.runasp.net/chathub',
+   notificationsUrl= 'http://prolance.runasp.net/notification',
+   biddingUrl= 'http://prolance.runasp.net/biddingHub'
+
   //  baseUrl= "http://localhost:5045/api/",
 
 }
 export enum Files {
-    filesUrl= "https://localhost:7093/files/",
+   // filesUrl= "https://localhost:7093/files/",
+    filesUrl= "http://prolance.runasp.net/files/",
+    // filesUrl= "http://localhost:5045/files/",
 }
 export enum projectImages {
-    filesUrl= "https://localhost:7093/projectimages",
+ //   filesUrl= "https://localhost:7093/projectimages",
+    filesUrl= "http://prolance.runasp.net/projectimages",
+    // filesUrl= "http://localhost:5045/projectimages",
 }
